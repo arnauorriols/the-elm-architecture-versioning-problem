84dfde0b6709b9f11e8ce0f092248f0c064d94dc
Arnau Orriols <dev@arnauorriols.com>
2018-02-18 20:34:31 +0100

    Implement alternative

--- src/A.elm
+++ src/A.elm
@@ -9,14 +9,15 @@ type alias Version =
     Int
 
 
-type Model
-    = A1Model A1.Model
-    | A2Model A2.Model
+type alias Model =
+    { version1 : A1.Model
+    , version2 : A2.Model
+    }
 
 
 init : Model
 init =
-    A1Model A1.init
+    { version1 = A1.init, version2 = A2.init }
 
 
 type Msg
@@ -26,34 +27,22 @@ type Msg
 
 update : Msg -> Model -> Model
 update msg model =
-    case ( msg, model ) of
-        ( A1Msg a1Msg, A1Model a1Model ) ->
-            A1.update a1Msg a1Model |> A1Model
+    case msg of
+        A1Msg a1Msg ->
+            { model | version1 = A1.update a1Msg model.version1 }
 
-        ( A2Msg a2Msg, A2Model a2Model ) ->
-            A2.update a2Msg a2Model |> A2Model
-
-        ( A2Msg _, A1Model _ ) ->
-            Debug.crash "msg A2 does not belong to model A1"
-
-        ( A1Msg _, A2Model _ ) ->
-            Debug.crash "msg A1 does not belong to model A2"
+        A2Msg a2Msg ->
+            { model | version2 = A2.update a2Msg model.version2 }
 
 
 view : Version -> Model -> Html.Html Msg
 view version model =
-    case ( version, model ) of
-        ( 1, A1Model a1Model ) ->
-            A1.view a1Model |> Html.map A1Msg
+    case version of
+        1 ->
+            A1.view model.version1 |> Html.map A1Msg
 
-        ( 2, A2Model a2Model ) ->
-            A2.view a2Model |> Html.map A2Msg
+        2 ->
+            A2.view model.version2 |> Html.map A2Msg
 
-        ( 1, A2Model a2Model ) ->
-            Html.text "Oops, requested version 1 but model is version 2!"
-
-        ( 2, A1Model a1Model ) ->
-            Html.text "Oops, requested version 2 but model is version 1!"
-
-        ( other, _ ) ->
+        other ->
             Html.text "Requested an unknown version of A"
